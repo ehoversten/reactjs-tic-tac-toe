0aeae35e70db10908faae2b94c6a35ca3a048591
Erik Hoversten <ehoversten@gmail.com>
2023-10-20 22:05:08 -0400

    updated check win logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,51 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Board from './components/Board'
 import Square from './components/Square'
 import './App.css'
 
 function App() {
-  const [gameState, setGameState] = useState(["", "", "", "", "", "", "", "", "circle"]);
+  const [gameState, setGameState] = useState(["", "", "", "", "", "", "", "", ""]);
   // Define turn state --> 'circle' | 'cross'
   const [turnState, setTurnState] = useState('circle');
+  const [winner, setWinner] = useState(null);
+
+  useEffect(() => {
+    checkWinConditions(gameState);
+  }, [gameState])
 
   console.log(gameState);
+  
+  const checkWinConditions = () => {
+    const winConditions = [
+      [0, 1, 2],
+      [0, 3, 6],
+      [2, 5, 8],
+      [6, 7, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+      [3, 4, 5],
+      [1, 4, 7]
+    ];
+   
+    winConditions.forEach(arr => {
+      let circleWin = arr.every(elem => gameState[elem] === 'circle');
+
+      if(circleWin) {
+        setWinner('circle');
+        return
+      }
+    });
+
+    winConditions.forEach(arr => {
+      let crossWin = arr.every(elem => gameState[elem] === 'cross');
+
+      if(crossWin) {
+        setWinner('cross');
+        return
+      }
+    })
+  }
+
   
   return (
     <>
